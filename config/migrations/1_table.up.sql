431ecd0eb5d9f48cfa6c86e91b506f08bff92c96
AvenidAcami <brrtgc1963@gmail.com>
2025-11-13 02:28:43 +0300

    added migrations

---
+++ config/migrations/1_table.up.sql
@@ -0,0 +1,34 @@
+CREATE TABLE public.members (
+    user_id text NOT NULL,
+    username text NOT NULL,
+    is_active boolean DEFAULT true NOT NULL,
+    team_name text
+);
+
+
+CREATE TABLE public.pull_requests (
+    pull_request_id bigint NOT NULL,
+    pull_request_name text NOT NULL,
+    author_id text NOT NULL,
+    status text NOT NULL,
+    "createdAt" time with time zone,
+    "mergedAt" time with time zone
+);
+
+
+CREATE TABLE public.teams (
+    team_name text NOT NULL
+);
+
+
+ALTER TABLE ONLY public.members
+    ADD CONSTRAINT members_pkey PRIMARY KEY (user_id);
+
+ALTER TABLE ONLY public.pull_requests
+    ADD CONSTRAINT pull_requests_pkey PRIMARY KEY (pull_request_id);
+
+ALTER TABLE ONLY public.teams
+    ADD CONSTRAINT teams_pkey PRIMARY KEY (team_name);
+
+ALTER TABLE ONLY public.members
+    ADD CONSTRAINT team FOREIGN KEY (team_name) REFERENCES public.teams(team_name) NOT VALID;
